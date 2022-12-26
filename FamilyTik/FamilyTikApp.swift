8d9c563d71bb68a89e6ff1924d1a660cff695b5c
amaal <amaal772@gmail.com>
2022-12-26 09:04:33 +0300

    Initial Commit

---
+++ FamilyTik/FamilyTikApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyTikApp.swift
+//  FamilyTik
+//
+//  Created by Amaal Almutairi on 02/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct FamilyTikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
